90dd54bf464c16e65f544b53462f6f3d38104c4b
Cédric David <cedric.david@btopenworld.com>
2018-12-07 23:27:04 +1000

    fix bug in loader comp"

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -1,21 +1,21 @@
 import React from "react";
-
 import { GridLoader } from "react-spinners";
 import { Box } from "gestalt";
 
 const Loader = ({ show }) =>
   show && (
     <Box
-      positon="fixed"
+      position="fixed"
       dangerouslySetInlineStyle={{
         __style: {
           bottom: 300,
           left: "50%",
-          transform: "translateX(50%)"
+          transform: "translateX(-50%)"
         }
       }}
     >
       <GridLoader color="darkorange" size={25} margin="3px" />
     </Box>
   );
+
 export default Loader;
